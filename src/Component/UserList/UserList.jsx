1b681de5a5ee35007432f3d5c1a21f46ea914de5
rohit <rohitsawant@gmail.com>
2023-10-10 11:32:45 +0530

    changes

--- src/Component/UserList/UserList.jsx
+++ src/Component/UserList/UserList.jsx
@@ -20,7 +20,7 @@ import { searchUser } from '../../Redux/Slice/UserSlice';
 import { debounce } from 'lodash'; // You need to install the lodash library
 
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.css';
 
 const UserList = () => {
     let dispatch = useDispatch()
